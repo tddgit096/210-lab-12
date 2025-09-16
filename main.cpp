d2d11ede7ab0dc8c1a782da5947196b4925827c4
tddgit096-pc <>
2025-09-16 13:48:50 -0700

    iterator sort, find element, max min

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //COMSC-210 | Lab 12: Pointers II | Toma Dimov
 //IDE used: VSC w/gbd compiler
 #include <iostream>
+#include <algorithm> 
 #include <fstream>
 #include <array>
 using namespace std;
@@ -42,7 +43,20 @@ int main(){
     cout<<"Address:" << sfx.data();
 
     //using iterators.
-    
+    sort(sfx.begin(),sfx.end());
+    cout<<"Array Sorted: ";
+    for(double val:sfx){cout<<val<<" "; cout<<endl;}
+    sort(sfx.end(),sfx.begin());
+    cout<<"Array Reverse Sorted: ";                 //sort in reverse
+    for(double val:sfx){cout<<val<<" "; cout<<endl;}
+
+    //find.
+    string target = "zap";
+    array<string,SFX_SIZE>::iterator it =find(sfx.begin(),sfx.end(),target); //make iterator and fill it with the find target (the element's location)
+    if(it!=sfx.end())   //not pointing to end means it is found
+        cout<<"Element \""<< target<<"\" found in position " << it - sfx.begin()<<endl;
+    else
+        cout<<"Element "<<target<<" not found.\n";
 
     return 0;
 }
