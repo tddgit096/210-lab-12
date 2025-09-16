1dc61db1e6544138991a16a4c519aa56d5ce4958
tddgit096-pc <>
2025-09-16 13:10:56 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//COMSC-210 | Lab 12: Pointers II | Toma Dimov
+//IDE used: VSC w/gbd compiler
+#include <iostream>
+using namespace std;
+
+/*
+Write a program that will exercise STD::array functions. Code a real-world simulation of something that could be simulated by an array. The requirements are:
+    *Use an STD::array
+    *Have an external data file with at least 30 elements in it
+    *Read the data into an STD::array 
+    *Use a large variety of the <array> member functions from this module to demonstrate your understanding of the STD::array.
+*/
+
+int main(){
+    return 0;
+}
